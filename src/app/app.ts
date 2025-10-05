a90475d261bd77c6d8d3b8277f753b0828f7ae2e
Antonio-DelaRua <ruxxdeveloper@gmail.com>
2025-10-05 13:03:17 +0200

    carrousel

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
+import { CarouselComponent } from "./components/carousel/carousel";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, CarouselComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/carousel/carousel.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router'; // Si usas routerLink en tu HTML
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.html',
+  styleUrls: ['./carousel.css'],
+  standalone: true, // Solo si quieres standalone
+  imports: [RouterModule,  NgFor], // Aquí SOLO van Componentes, Directivas, Pipes, o NgModules
+})
+export class CarouselComponent {
+  slides = [
+    {
+      img: 'assets/slider-main/bg1.jpg',
+      badge: 'GONAN BUILDING CONSTRUCCIONES',
+      subtitle: 'TE LO PONEMOS FACIL',
+      title: 'ELIGE LOS MEJORES',
+      titleHighlight: 'PROFESIONALES',
+      btnText: 'ELIGE NUESTROS SERVICIOS',
+      btnLink: '/services'
+    },
+    // Puedes añadir más slides si quieres
+    {
+      img: 'assets/slider-main/bg1.jpg',
+      badge: 'GONAN BUILDING CONSTRUCCIONES',
+      subtitle: 'TE LO PONEMOS FACIL',
+      title: 'ELIGE',
+      titleHighlight: 'PROFESIONALES',
+      btnText: 'ELIGE NUESTROS SERVICIOS',
+      btnLink: '/services'
+    },
+    {
+      img: 'assets/slider-main/bg1.jpg',
+      badge: 'GONAN BUILDING CONSTRUCCIONES',
+      subtitle: 'TE LO PONE',
+      title: 'ELIGE LOS MEJORES',
+      titleHighlight: 'PROFESIONALES',
+      btnText: 'ELIGE NUESTROS SERVICIOS',
+      btnLink: '/services'
+    },
+  ];
+  current = 0;
+  prev() {
+    this.current = (this.current - 1 + this.slides.length) % this.slides.length;
+  }
+  next() {
+    this.current = (this.current + 1) % this.slides.length;
+  }
+}
